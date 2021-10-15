e443c15c478b1c24d190148512ce709b8f340415
Dante Williams <Danteawilliams@gmail.com>
2021-10-14 20:09:59 -0400

    initial commit

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../Images/logo.svg';
+
+const navbarStyle = {
+  backgroundColor: 'Pink',
+};
+
+const Header = ({ title }) => {
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Logo style={{ maxWidth: '12rem', maxHeight: '3rem' }} />
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
